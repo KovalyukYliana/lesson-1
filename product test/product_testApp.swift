9f05acc22b25fb0771169f86b4b097ce49ec7e83
KovalyukYliana <m2202457@edu.misis.ru>
2023-10-02 16:49:15 +0300

    Initial Commit

---
+++ product test/product_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  product_testApp.swift
+//  product test
+//
+//  Created by Yliana Kovalyuk on 02.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct product_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
